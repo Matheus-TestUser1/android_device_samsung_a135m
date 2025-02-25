e96e049e57f082e081b6afc13ab05c2412a3fded
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2025-02-25 10:57:16 -0300

    Update device.mk
    Signed-off-by: Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -24,3 +24,9 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+# Vendor Security firmware files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vendor/firmware/rxse.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/rxse.bin \
+    $(LOCAL_PATH)/vendor/firmware/txse1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse1.bin \
+    $(LOCAL_PATH)/vendor/firmware/txse2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse2.bin \
+    $(LOCAL_PATH)/vendor/firmware/sb_others.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/sb_others.bin
