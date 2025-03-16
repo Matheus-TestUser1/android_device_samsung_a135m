a3a2a88fa90f56180883520005da208ad07bc61a
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2025-03-15 22:44:34 -0300

    Add files via upload
    Signed-off-by: Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>

--- recovery/root/system/bin/detect_touch.sh
+++ recovery/root/system/bin/detect_touch.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 # Touch Detection Script for Samsung Galaxy A13
-# Updated: 2025-03-16 01:31:05
-# Author: Matheus-TestUser1
+# Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): 2025-03-16 01:41:52
+# Current User's Login: Matheus-TestUser1
 
 # Check touch hardware
 touch_id=$(cat /sys/class/sec/tsp/device/id)
@@ -9,46 +9,46 @@ touch_id=$(cat /sys/class/sec/tsp/device/id)
 case "$touch_id" in
     "HX83*")
         # Himax
-        setprop ro.recovery.touch.driver "himax_ts"
+        setprop persist.sys.touch.driver "himax_ts"
         ;;
     "SEC_TS*")
         # Samsung
-        setprop ro.recovery.touch.driver "sec_ts"
+        setprop persist.sys.touch.driver "sec_ts"
         ;;
     "NVT*")
         # Novatek
-        setprop ro.recovery.touch.driver "novatek_ts"
+        setprop persist.sys.touch.driver "novatek_ts"
         ;;
     "FT*")
         # Focaltech
-        setprop ro.recovery.touch.driver "focaltech_ts"
+        setprop persist.sys.touch.driver "focaltech_ts"
         ;;
     "GT*")
         # Goodix
-        setprop ro.recovery.touch.driver "goodix_ts"
+        setprop persist.sys.touch.driver "goodix_ts"
         ;;
     "S3*")
         # Synaptics
-        setprop ro.recovery.touch.driver "synaptics_ts"
+        setprop persist.sys.touch.driver "synaptics_ts"
         ;;
     "EG*")
         # eGalax
-        setprop ro.recovery.touch.driver "egalax_ts"
+        setprop persist.sys.touch.driver "egalax_ts"
         ;;
     "AT*")
         # Atmel
-        setprop ro.recovery.touch.driver "atmel_ts"
+        setprop persist.sys.touch.driver "atmel_ts"
         ;;
     "MF*")
         # Melfas
-        setprop ro.recovery.touch.driver "melfas_ts"
+        setprop persist.sys.touch.driver "melfas_ts"
         ;;
     "IM*")
         # Imagis
-        setprop ro.recovery.touch.driver "imagis_ts"
+        setprop persist.sys.touch.driver "imagis_ts"
         ;;
     *)
         # Default to Samsung
-        setprop ro.recovery.touch.driver "sec_ts"
+        setprop persist.sys.touch.driver "sec_ts"
         ;;
 esac
